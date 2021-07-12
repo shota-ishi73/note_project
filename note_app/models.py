aec2a7e4c0339e658b35c19006a3b6e8d1b356cc
shota ishi <1473shota@gmail.com>
2021-07-13 07:40:22 +0900

    ログイン機能　補足

--- note_app/models.py
+++ note_app/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from user.models import User
 
 # Create your models here.
 class Note(models.Model):
+  user_id = models.ForeignKey(
+    User, on_delete=models.RESTRICT
+  )
   title = models.CharField('タイトル',max_length=200)
   text = models.TextField('本文')
   category = models.CharField('分類',max_length=200)
